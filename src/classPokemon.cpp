1e2ca9f764fb23c41d90bd23de1212a5e7263d2b
Paul Ruwisch <pruwisch@sievers-group.com>
2024-10-12 18:15:42 +0200

    more moves

--- src/classPokemon.cpp
+++ src/classPokemon.cpp
@@ -1,16 +1,28 @@
 #include "classPokemon.h"
 
 //Constructor
-Pokemon::Pokemon(std::string name, int attack, int defense, int speed) {
+Pokemon::Pokemon(std::string name, int hpmax, int hp, int attack, int defense, int speed, Attack m1, Attack m2, Attack m3, Attack m4) {
     setPokemonName(name);
+    setHealthPointsMax(hpmax);
+    setHealthPoints(hp);
     setAttackStat(attack);
     setDefenseStat(defense);
     setSpeedStat(speed);
+    setMove1(m1);
+    setMove2(m2);
+    setMove3(m3);
+    setMove4(m4);
 }
 
 //Getters
 std::string Pokemon::getName() {
     return pokemonName;
+}
+int getHealthPointsMax() {
+    return healthPointsMax;
+}
+int getHealthPoints() {
+    return healthPoints;
 }
 int Pokemon::getAttackStat() {
     return attackStat;
@@ -20,6 +32,18 @@ int Pokemon::getDefenseStat() {
 }
 int Pokemon::getSpeedStat() {
     return speedStat;
+}
+Attack getMove1() {
+    return move1;
+}
+Attack getMove2() {
+    return move2;
+}
+Attack getMove3() {
+    return move3;
+}
+Attack getMove4() {
+    return move4;
 }
 
 //Setters
@@ -34,4 +58,16 @@ void Pokemon::setDefenseStat(int x) {
 }
 void Pokemon::setSpeedStat(int x) {
     speedStat = x;
+}
+void Pokemon::setMove1(Attack move) {
+    move1 = move;
+}
+void Pokemon::setMove2(Attack move) {
+    move2 = move;
+}
+void Pokemon::setMove3(Attack move) {
+    move3 = move;
+}
+void Pokemon::setMove4(Attack move) {
+    move4 = move;
 }
--- src/classPokemon.h
+++ src/classPokemon.h
@@ -2,33 +2,52 @@
 #define CLASSPOKEMON_H
 
 #include <string>
+#include "classAttack.h"
 
 
 class Pokemon {
 
     private:
         std::string pokemonName;
+        int healthPointsMax;
+        int healthPoints;
         int attackStat;
         int defenseStat;
         int speedStat;
-        
+        Attack move1;
+        Attack move2;
+        Attack move3;
+        Attack move4;
+
         
     public:
-    //Constructor
-        Pokemon(std::string name, int attack, int defense, int speed);
+        //Constructor
+        Pokemon(std::string name, int hpmax, int hp, int attack, int defense, int speed, Attack m1, Attack m2, Attack m3, Attack m4);
 
     
         //Getters
         std::string getName();
+        int getHealthPointsMax();
+        int getHealthPoints();
         int getAttackStat();
         int getDefenseStat();
         int getSpeedStat();
+        Attack getMove1();
+        Attack getMove2();
+        Attack getMove3();
+        Attack getMove4();
 
         //Setters
         void setPokemonName(std::string name);
+        void setHealthPointsMax(int x);
+        void setHealthPoints(int x);
         void setAttackStat(int x);
         void setDefenseStat(int x);
         void setSpeedStat(int x);
+        void setMove1(Attack);
+        void setMove2(Attack);
+        void setMove3(Attack);
+        void setMove4(Attack);
         
 
 };
