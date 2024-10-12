291ec2959c22f88b50606a5166afffacfbc5b632
Paul Ruwisch <pruwisch@sievers-group.com>
2024-10-12 14:06:52 +0200

    more Header shit

--- classPokemon.cpp
+++ classPokemon.cpp
@@ -9,29 +9,29 @@ Pokemon::Pokemon(std::string name, int attack, int defense, int speed) {
 }
 
 //Getters
-        std::string getName() {
-            return pokemonName;
-        }
-        int getAttackStat() {
-            return attackStat;
-        }
-        int getDefenseStat() {
-            return defenseStat;
-        }
-        int getSpeedStat() {
-            return speedStat;
-        }
+std::string getName() {
+    return pokemonName;
+}
+int getAttackStat() {
+    return attackStat;
+}
+int getDefenseStat() {
+    return defenseStat;
+}
+int getSpeedStat() {
+    return speedStat;
+}
 
-        //Setters
-        void setPokemonName(std::string name) {
-            pokemonName = name;
-        }
-        void setAttackStat(int x) {
-            attackStat = x;
-        }
-        void setDefenseStat(int x) {
-            defenseStat = x;
-        }
-        void setSpeedStat(int x) {
-            speedStat = x;
-        }
+//Setters
+void setPokemonName(std::string name) {
+    pokemonName = name;
+}
+void setAttackStat(int x) {
+    attackStat = x;
+}
+void setDefenseStat(int x) {
+    defenseStat = x;
+}
+void setSpeedStat(int x) {
+    speedStat = x;
+}
